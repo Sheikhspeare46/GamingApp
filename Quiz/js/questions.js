4420b9f4a49385defbe9db3e3154a3c0f0b2abe8
Sheikhspeare46 <naeem150702@gmail.com>
2024-06-01 14:48:53 +0530

    hello

---
+++ Quiz/js/questions.js
@@ -0,0 +1,112 @@
+let questions = [
+  {
+    numb: 1,
+    question: "What does IDE stand for?",
+    answer: "Integrated Development Environment",
+    options: [
+      "Integrated Developer Environment",
+      "Integrated Design Environment",
+      "Integrated Development Engine",
+      "Integrated Deployment Environment"
+    ]
+  },
+  {
+    numb: 2,
+    question: "What does API stand for?",
+    answer: "Application Programming Interface",
+    options: [
+      "Application Program Interface",
+      "Automated Programming Interface",
+      "Automated Program Interface",
+      "Application Processing Interface"
+    ]
+  },
+  {
+    numb: 3,
+    question: "What does URL stand for?",
+    answer: "Uniform Resource Locator",
+    options: [
+      "Uniform Resource Link",
+      "Uniform Resource Language",
+      "Universal Resource Locator",
+      "Universal Resource Link"
+    ]
+  },
+  {
+    numb: 4,
+    question: "What does HTTP stand for?",
+    answer: "Hypertext Transfer Protocol",
+    options: [
+      "Hypertext Transmission Protocol",
+      "Hypertext Terminal Protocol",
+      "Hypertext Transport Protocol",
+      "Hypertext Transfer Process"
+    ]
+  },
+  {
+    numb: 5,
+    question: "What does CSS stand for?",
+    answer: "Cascading Style Sheets",
+    options: [
+      "Cascading Style Syntax",
+      "Cascading Style System",
+      "Cascading Style Selector",
+      "Cascading Style Sheets"
+    ]
+  },
+  {
+    numb: 6,
+    question: "What does HTML stand for?",
+    answer: "Hyper Text Markup Language",
+    options: [
+      "Hyper Text Multi Language",
+      "Hyperlinks and Text Markup Language",
+      "Home Tool Markup Language",
+      "Hyper Text Markup Language"
+    ]
+  },
+  {
+    numb: 7,
+    question: "What does API stand for?",
+    answer: "Application Programming Interface",
+    options: [
+      "Automated Program Interface",
+      "Application Program Interface",
+      "Automated Programming Interface",
+      "Application Processor Interface"
+    ]
+  },
+  {
+    numb: 8,
+    question: "What does DOM stand for?",
+    answer: "Document Object Model",
+    options: [
+      "Document Object Method",
+      "Document Order Model",
+      "Data Object Model",
+      "Document Object Model"
+    ]
+  },
+  {
+    numb: 9,
+    question: "What does JSON stand for?",
+    answer: "JavaScript Object Notation",
+    options: [
+      "JavaScript Object Notation",
+      "JavaScript Object Network",
+      "JavaScript Order Notation",
+      "JavaScript Object Naming"
+    ]
+  },
+  {
+    numb: 10,
+    question: "What does JVM stand for?",
+    answer: "Java Virtual Machine",
+    options: [
+      "Java Virtual Machine",
+      "Java Virtual Model",
+      "Java Variable Machine",
+      "JavaScript Virtual Machine"
+    ]
+  },
+];
---
+++ word scramble/js/words.js
@@ -0,0 +1,54 @@
+let words = [
+    {
+        word: "addition",
+        hint: "The process of adding numbers"
+    },
+    {
+        word: "number",
+        hint: "Math symbol used for counting"
+    },
+    {
+        word: "garden",
+        hint: "Space for planting flower and plant"
+    },
+    {
+        word: "feather",
+        hint: "Hair like outer covering of bird"
+    },
+    {
+        word: "comfort",
+        hint: "A pleasant feeling of relaxation"
+    },
+    {
+        word: "tongue",
+        hint: "The muscular organ of mouth"
+    },
+    {
+        word: "expansion",
+        hint: "The process of increase or grow"
+    },
+    {
+        word: "group",
+        hint: "A number of objects or persons"
+    },
+    {
+        word: "python",
+        hint: "Programming language and also a species of snakes"
+    },
+    {
+        word: "needle",
+        hint: "A thin and sharp metal pin"
+    },
+    {
+        word: "expert",
+        hint: "Person with extensive knowledge"
+    },
+    {
+        word: "second",
+        hint: "One-sixtieth of a minute"
+    },
+    {
+        word: "library",
+        hint: "Place containing collection of books"
+    },
+]
